03fd5f12fbfe9c781c69ce0a65b12cde8807cda8
Pravesh-mansharamani <praveshramani295@gmail.com>
2024-02-28 13:59:11 -0700

    fixed nextjs problem

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen bg-black">
+      <Header />
+      <div>
+        MR GOOSE
+      </div>
+    </main>
+    )
+}
