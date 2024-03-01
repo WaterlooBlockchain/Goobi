82e6f293baa935c9a52fb29c5b77326968b16ce8
Pravesh-mansharamani <praveshramani295@gmail.com>
2024-03-01 12:34:07 -0700

    email end point

--- frontend/src/components/Email.tsx
+++ frontend/src/components/Email.tsx
@@ -1,4 +1,28 @@
+
+
 export default function Email() {
+
+    const handleSubmit = async () => {
+        const emailInput = document.querySelector('input[type="email"]') as HTMLInputElement;
+        const email = emailInput ? emailInput.value : () => { alert('Empty email!'); return;} ;
+        const res = await fetch('/api/email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email }),
+        });
+        if (res.status === 200) {
+            alert('Email submitted successfully');
+        } else if (res.status === 400) {
+            alert('Email already submitted');
+        } else {
+            alert('Internal server error');
+        }
+
+    };
+
+
     return (
         <section className="py-8 m-auto">
         <h2 className="text-3xl font-bold mb-4">Submit Email to Claim</h2>
@@ -10,7 +34,7 @@ export default function Email() {
         className="rounded-l-md px-4 py-2 border border-gray-300 focus:outline-none focus:border-blue-500 flex-1 text-black"
         required // Ensure that the field is not empty
         />
-      <button className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-r-md">
+      <button className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-r-md" onClick={handleSubmit}>
         Submit
       </button>
         </div>
