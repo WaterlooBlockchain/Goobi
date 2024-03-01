4fb007424d7b2038bcbee7ba877da553863191e1
mj <mjk4534@ewhain.net>
2024-03-01 14:58:49 -0700

    landing page update

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,22 +2,20 @@ import Image from 'next/image';
 import mrgoose from '../../public/mr-goose.jpg';
 
 export default function Hero() {
-    return (
-       
-
-<div className="flex items-center justify-between p-8 text-white">
-{/* Left section with Mr. Goose image */}
-<div className="flex items-center">
-  <div className="w-5/6 h-5/6">
-    <Image src={mrgoose} alt="Mr. Goose" layout="responsive" />
-  </div>
-</div>
+  return (
+    <div className="flex items-stretch p-8 text-white"> {/* Change items-center to items-stretch */}
+      {/* Left section with Mr. Goose image */}
+      <div className="w-1/3 flex items-center justify-center"> {/* Adjust the width as needed */}
+        <div className="w-full h-full">
+          <Image src={mrgoose} alt="Mr. Goose" layout="responsive" width={200} height={200} /> {/* Adjust width and height as needed */}
+        </div>
+      </div>
 
       {/* Right section with text */}
-      <div className="w-full h-5/6 bg-gradient-to-r from-yellow-300 to-zinc-900">
-<div className="text-4xl text-yellow-400 font-bold mb-4">Introducing $MRGOOSE</div>
-        <div className="text-3xl font-semibold mb-4 text-yellow-700"> A token for <br /> Waterloo Blockchain</div>
+      <div className="flex flex-col justify-center flex-1 px-8 py-4 bg-gradient-to-r from-yellow-300 via-yellow-600 to-zinc-900"> {/* Remove h-full class */}
+        <div className="text-4xl text-black font-bold mb-8">Introducing $MRGOOSE</div>
+        <div className="text-3xl animate-pulse font-semibold mb-4 text-yellow-700">A token for <br />Waterloo Blockchain</div>
       </div>
     </div>
-    )
+  );
 }
