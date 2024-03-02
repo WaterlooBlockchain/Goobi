bfd36b2fd68a9d0e08d933cb0e03cb81a0b6f925
Amy Qin <amyqin501@gmail.com>
2024-03-01 21:53:45 -0700

    Fix ticker

--- frontend/src/components/Merch.tsx
+++ frontend/src/components/Merch.tsx
@@ -6,7 +6,7 @@ const ProductCard: React.FC<{ product: any }> = ({ product }) => (
   <div className="border bg-white border-gray-200 p-4 rounded-md">
     <img src={product.image} alt={product.name} className="w-full h-32 object-cover mb-2" />
     <h3 className="text-lg font-bold">{product.name}</h3>
-    <p className="text-gray-600">${product.cost}</p>
+    <p className="text-gray-600">{product.cost} $MRGOOSE</p>
   </div>
 );
 
@@ -24,7 +24,6 @@ async function Merch() {
 
 
   const {data: products}: any = await strapi.find("merches");
-  console.log(products)
 
   return (
     <div className="container mx-auto py-8">
