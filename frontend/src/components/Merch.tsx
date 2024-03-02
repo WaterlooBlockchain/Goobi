a9a0a5005a4ac87a2c9d73603e46a22bfc6739e1
Amy Qin <amyqin501@gmail.com>
2024-03-01 21:52:07 -0700

    Add merch api

--- frontend/src/components/Merch.tsx
+++ frontend/src/components/Merch.tsx
@@ -1,33 +1,37 @@
 // Merch.tsx
 import React from 'react';
+import Strapi from 'strapi-sdk-js';
 
-// Example product data (replace this with your actual data)
-const products = [
-  { id: 1, name: 'Product 1', price: 10, image: '/product1.jpg' },
-  { id: 2, name: 'Product 2', price: 20, image: '/product2.jpg' },
-  { id: 3, name: 'Product 3', price: 30, image: '/product3.jpg' },
-  // Add more products as needed
-];
-
-const ProductCard: React.FC<{ product: { id: number, name: string, price: number, image: string } }> = ({ product }) => (
+const ProductCard: React.FC<{ product: any }> = ({ product }) => (
   <div className="border bg-white border-gray-200 p-4 rounded-md">
     <img src={product.image} alt={product.name} className="w-full h-32 object-cover mb-2" />
     <h3 className="text-lg font-bold">{product.name}</h3>
-    <p className="text-gray-600">${product.price}</p>
+    <p className="text-gray-600">${product.cost}</p>
   </div>
 );
 
-const Merch: React.FC = () => {
-  // Render only the first three products
-  const displayedProducts = products.slice(0, 3);
+async function Merch() {
+  const strapi = new Strapi({
+    url: process.env.BACKEND_URL,
+    prefix: "/api",
+    store: {
+      key: "strapi_jwt",
+      useLocalStorage: false,
+      cookieOptions: { path: "/" },
+    },
+    axiosOptions: {},
+  })
+
+
+  const {data: products}: any = await strapi.find("merches");
+  console.log(products)
 
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-4 text-yellow-400">Shop Merchandise</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {displayedProducts.map(product => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+        { products.map((product: any) => <ProductCard key={product.id} product={product.attributes} />)
+        }
       </div>
     </div>
   );
