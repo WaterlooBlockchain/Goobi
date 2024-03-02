aab15b4b732921cf058848c1dfa1cf657504e7cc
Amy Qin <amyqin501@gmail.com>
2024-03-02 08:37:59 -0700

    Add wallet connect

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,15 +1,17 @@
-import { Montserrat } from "next/font/google";
 import Link from "next/link";
 import Image from "next/image";
 import transparentGoose from "../public/transparent_goose.png"
 import waterlooBlockchainLogo from '../public/waterloo_blockchain_logo.png'
 import Merch from './Merch';
+import dynamic from "next/dynamic";
 
-const inter = Montserrat({ subsets: ["latin"] });
-
+const WalletMultiButtonDynamic = dynamic(
+  async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+  { ssr: false }
+);
 export default function Header() {
   return (
-<header className="flex justify-between items-center px-8 lg:px-64 py-12">
+    <header className="flex justify-between items-center px-8 lg:px-64 py-12">
       <div className="flex flex-row items-center justify-center">
         <Image src={transparentGoose} alt="Mr. Goose" width={100} height={100} />
         <Image src={waterlooBlockchainLogo} alt="Mr. Goose" width={150} height={100} />
@@ -21,6 +23,7 @@ export default function Header() {
         <Link href="/merch" passHref  legacyBehavior>
           <a className="text-lg lg:text-2xl text-yellow-200 font-bold  hover:text-yellow-500 underline">Merch</a>
         </Link>
+        <WalletMultiButtonDynamic />
       </nav>
     </header>
   )
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,9 +2,38 @@
 
 import "@/styles/globals.css";
 import type { AppProps } from 'next/app';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { useMemo } from 'react';
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
 
 export default function App({ Component, pageProps}: AppProps) {
+    const network = 'https://nextjs.org/docs/messages/next-image-upgrade-to-13';
+    console.log(network)
+    const endpoint = useMemo(() => network, [network]);
+    console.log(endpoint)
+
+
+    const wallets = useMemo(
+        () => [
+          new SolflareWalletAdapter(),
+          new PhantomWalletAdapter()
+        ],
+        [network]
+    );
+    
     return (
-        <Component {...pageProps} />
+        <>
+            <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets}>
+                <WalletModalProvider>
+                <Component {...pageProps} />
+                    </WalletModalProvider>
+                </WalletProvider>
+        </ConnectionProvider>
+
+        </>
     )
 }
